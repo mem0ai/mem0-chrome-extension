5ffcce98f8f4ce73abd1e215a44e49f542f1941d
Vikram Iyer <vikramiyer@mac.local.meter>
2025-09-03 17:33:36 -0700

    fixed extension icon click

--- background.js
+++ background.js
@@ -1,36 +1,10 @@
 import { initContextMenuMemory } from './context-menu-memory.js';
 import { initDirectUrlTracking } from './direct-url-tracker.js';
-import { sendExtensionEvent, getBrowser } from './utils/util_functions.js';
-
-chrome.action.onClicked.addListener((tab) => {
-  sendExtensionEvent("extension_browser_icon_clicked", {
-    browser: getBrowser(),
-    source: "OPENMEMORY_CHROME_EXTENSION",
-    tab_url: tab.url
-  });
-
-  // Check auth status and open popup or toggle sidebar
-  chrome.storage.sync.get(["apiKey", "access_token"], function (data) {
-    if (data.apiKey || data.access_token) {
-      chrome.tabs.sendMessage(tab.id, { action: "toggleSidebar" });
-    } else {
-      chrome.action.openPopup();
-    }
-  });
-});
-
 
 // Initial setting when extension is installed or updated
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener((details) => {
   chrome.storage.sync.set({ memory_enabled: true }, function() {
     console.log('Memory enabled set to true on install/update');
-
-    if (details.reason === 'install') {
-      sendExtensionEvent("extension_installed", {
-        browser: getBrowser(),
-        source: "chrome_web_store"
-      });
-    }
   });
 });
 
